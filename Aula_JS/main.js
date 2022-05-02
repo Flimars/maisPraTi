5e7dcf6b76c589a09606b67dee67164bcdac1339
flimars <flavio.lima@aluno.riogrande.ifrs.edu.br>
2022-05-02 15:51:06 -0300

    Primeiros passos com JS.

---
+++ Aula_JS/main.js
@@ -0,0 +1,30 @@
+var nome = 'Chico Bento';
+var idade = 30;
+var outraIdade = 25;
+alert("Bem-vindo!, "+ nome);  // Bem-Vindo!, Chico Bento
+// Vai interpretar as variaveis com valores inteiros e somar.
+alert(idade + outraIdade);  // 55
+
+var nome2 = 'Rosa Maria (Rosinha)';
+var idade2 = "27";
+var outraIdade2 = "25";
+// Vai interpretar as variaveis como strings e concatenar.
+alert(idade2 + outraIdade2); // 2725
+
+
+/* Testando a função Alert() */
+var i = 1;
+alert(i);
+i =i + 1;
+alert(i);
+
+alert("i");
+alert(i);
+
+i =i + 5;
+alert(i);
+
+// Usando Operador Condicional Ternário.
+var x = 5;
+var y = 12;
+var z = x > 10 ? alert("X é maior que 10.") : alert("X é menor que 10.");
